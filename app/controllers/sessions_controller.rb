10629079fc6ec138eba250c58094f38283adeb95
96StanleyLee <96stanleylee@gmail.com>
2020-03-22 21:24:50 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+
+
+   def create
+
+    if params[:name] == nil || params[:name].empty?
+        redirect_to '/login'
+    else
+        session[:name] = params[:name]
+    end 
+   end 
+
+
+   def destroy 
+    session.delete :name
+
+   end 
+
+end
+  
